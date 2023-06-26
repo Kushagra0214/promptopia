9ff5bc82e290a80837381ae058ae0987b852ecb8
Kushagra Srivastava <kushagrasrivastava908@gmail.com>
2023-06-26 16:40:35 +0530

    Updating home page for desktop

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import { Children } from "react";
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover and Share AI Prompts from anywhere in the world to everywhere."
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+            <div className="gradient"></div>
+        </div>
+
+        <main className="app">
+            <Nav />
+            {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
